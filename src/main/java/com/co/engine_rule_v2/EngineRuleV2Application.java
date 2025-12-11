1e1fe8faff3bd9e767028db2f44f7d9a267670d3
juzamora_cib <juzamora@bancolombia.com.co>
2025-12-11 12:09:34 -0500

    feat(): Proyecto base para prueba de concepto de motor de reglas

---
+++ src/main/java/com/co/engine_rule_v2/EngineRuleV2Application.java
@@ -0,0 +1,13 @@
+package com.co.engine_rule_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineRuleV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineRuleV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/engine_rule_v2/EngineRuleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.engine_rule_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineRuleV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
